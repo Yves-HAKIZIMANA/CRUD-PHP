a7a983ad043d480c8b388193df2517c53eb39673
Yves <hkzmnyves1@gmail.com>
2023-04-13 20:09:30 +0200

    Crud operation in php

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the create , read , update and delete operations in php
---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php 
+
+include "config.php"; 
+
+if (isset($_GET['id'])) {
+
+    $user_id = $_GET['id'];
+
+    $sql = "DELETE FROM `users` WHERE `id`='$user_id'";
+
+     $result = $conn->query($sql);
+
+     if ($result == TRUE) {
+
+        echo "Record deleted successfully.";
+
+    }else{
+
+        echo "Error:" . $sql . "<br>" . $conn->error;
+
+    }
+
+} 
+
+?>
+
